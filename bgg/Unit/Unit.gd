e6cb11de681ebb8a84cd8055f242a6a3115c6887
R. Stephens Summerlin <st.summerlin@gmail.com>
2017-09-23 20:12:33 -0400

    Added ghost and mouse following

--- bgg/Unit/Unit.gd
+++ bgg/Unit/Unit.gd
@@ -1,18 +1,31 @@
 extends Node2D
 
+enum STATE {
+  None,
+  Moving
+}
+
 var IsSelected = false
+var state = STATE.None
+onready var ghost = get_node('Ghost')
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process(true)
+
+func _process(delta):
+	if state == STATE.Moving:
+		ghost.set_global_pos(get_viewport().get_mouse_pos())
 
 func Select():
 	IsSelected = true
 	get_node('Unit Marker/Sprite').set_modulate(Color('f6ff00')) # Yellow
+	state = STATE.Moving
+	ghost.show()
 	print('Selected ' + get_name())
 
 func Deselect():
 	IsSelected = false
+	state = STATE.None
+	ghost.hide()
 	get_node('Unit Marker/Sprite').set_modulate('ffffff') # White
 	print('Deselected ' + get_name())
