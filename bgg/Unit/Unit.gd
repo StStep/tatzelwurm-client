a606ff2dae9397bbca01cc4d91bd8e57db84a0b9
R. Stephens Summerlin <st.summerlin@gmail.com>
2017-09-23 23:53:58 -0400

    Added use of state for unit

--- bgg/Unit/Unit.gd
+++ bgg/Unit/Unit.gd
@@ -27,32 +27,71 @@ func _process(delta):
 
 
 func _on_miss_click(button):
-	if button == BUTTON_LEFT or button == BUTTON_RIGHT:
-		if IsSelected:
-			get_node('/root/game_manager').DeselectUnit()
+	# Deselect
+	if state == STATE.None:
+		if button == BUTTON_LEFT or button == BUTTON_RIGHT:
+			if IsSelected:
+				get_node('/root/game_manager').DeselectUnit()
+		else:
+			pass
+	# Add Move or Deselect
+	elif state == STATE.Moving:
+		if button == BUTTON_LEFT:
+			print('Add move')
+		elif button == BUTTON_RIGHT:
+			ChangeState(STATE.None)
+		else:
+			pass
 	else:
 		pass
 
-
+# TODO Use Match?
 func _on_marker_click(button):
-	if button == BUTTON_LEFT:
-		 get_node('/root/game_manager').selUnit = self
+	# Select or start moving if selected
+	if state == STATE.None:
+		if IsSelected:
+			ChangeState(STATE.Moving)
+		else:
+			if button == BUTTON_LEFT:
+				get_node('/root/game_manager').selUnit = self
+	elif state == STATE.Moving:
+		pass
 	else:
 		pass
 
+func ChangeState(s):
+	if s == state:
+		return
+
+	# Prev State
+	if state == STATE.None:
+		pass
+	elif state == STATE.Moving:
+		ghost.hide()
+	else:
+		pass
+
+	# New State
+	if s == STATE.None:
+		pass
+	elif s == STATE.Moving:
+		ghost.show()
+	else:
+		pass
+
+	state = s
 
 func Select():
 	IsSelected = true
 	get_node('Marker/Sprite').set_modulate(Color('f6ff00')) # Yellow
-	state = STATE.Moving
-	ghost.show()
 	print('Selected ' + get_name())
 
 
 func Deselect():
-	IsSelected = false
-	state = STATE.None
-	ghost.hide()
-	get_node('Marker/Sprite').set_modulate('ffffff') # White
-	print('Deselected ' + get_name())
-	return true
+	if state == STATE.None:
+		IsSelected = false
+		get_node('Marker/Sprite').set_modulate('ffffff') # White
+		print('Deselected ' + get_name())
+		return true
+	else:
+		return false
