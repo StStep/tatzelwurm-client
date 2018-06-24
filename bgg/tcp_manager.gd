c6550d0d8bf019d65ea1d1a1798a3672d3106599
R. Stephens Summerlin <st.summerlin@gmail.com>
2018-06-24 17:54:31 -0400

    Modified server data print to print String

--- bgg/tcp_manager.gd
+++ bgg/tcp_manager.gd
@@ -4,7 +4,7 @@ const PORT = 1337
 const HOST = "127.0.0.1"
 var client = StreamPeerTCP.new()
 
-const TIMEOUT = 3
+const TIMEOUT = 3.0
 
 var timeout_counter = 0
 var connect_attemps_counter = 0
@@ -12,7 +12,6 @@ var connected = false
 
 func _ready():
 	set_process(true)
-	pass
 
 func _process(delta):
 	match client.get_status():
@@ -20,6 +19,7 @@ func _process(delta):
 			if connected:
 				print('TCP: Disonnected')
 				connected = false;
+
 			if timeout_counter == 0:
 				connect_attemps_counter += 1
 				print("TCP: Trying to Connect to ",HOST,":",PORT," (",connect_attemps_counter,")")
@@ -28,7 +28,6 @@ func _process(delta):
 				timeout_counter += delta
 				if timeout_counter >= TIMEOUT:
 					timeout_counter = 0 # we would like to try to connect again
-			pass
 		StreamPeerTCP.STATUS_CONNECTING:
 			pass
 		StreamPeerTCP.STATUS_CONNECTED:
@@ -38,5 +37,4 @@ func _process(delta):
 			var bytes = client.get_available_bytes()
 			if bytes > 0:
 				var data = client.get_data(bytes)
-				print('TCP: RX, %d bytes - %s' % [bytes, data])
-			pass
+				print('TCP: RX, %d bytes - Error %d Data "%s"' % [bytes, data[0], data[1].get_string_from_utf8()])
