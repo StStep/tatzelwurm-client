4619523c147a5209c694570a925aa779cd99b35d
R. Stephens Summerlin <st.summerlin@gmail.com>
2019-10-12 21:31:02 -0400

    Using c# position node, connections disabled for now, crashing

---
+++ bgg/Unit/PositionNode.cs
@@ -0,0 +1,120 @@
+using Godot;
+using System;
+
+public class PositionNode : Node2D
+{
+    public Node2D unit { get; set; }
+    public Vector2 start => previous != null ? previous.end : unit.GlobalPosition;
+
+    public Vector2 end
+    {
+        get => ToGlobal(Vector2.Zero);
+        set
+        {
+            move = value - start;
+            GlobalPosition = value;
+            GlobalRotation = move.Angle() + (float)(Mathf.Pi/2.0);
+            update();
+        }
+    }
+
+    public Color C_NOT_HIGHLIGHTED = new Color("ffffff"); // White
+    public Color C_HIGHLIGHT = new  Color("b6ff00"); // Green-Yellow
+
+    public MouseArea2d marker;
+    public Line2D path;
+    public MouseArea2d path_area;
+    public CollisionShape2D path_shape;
+
+    public PositionNode previous = null;
+    public PositionNode next = null;
+
+    public Vector2 move = Vector2.Zero;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        marker = GetNode<MouseArea2d>("Marker");
+        path = GetNode<Line2D>("Path");
+        path_area = GetNode<MouseArea2d>("PathArea");
+        path_shape = GetNode<CollisionShape2D>("PathArea/Shape");
+
+	    //marker.Connect(nameof(MouseArea2d.mouse_hover_changed), this, nameof(_render_marker_highlight));
+	    //marker.Connect(nameof(MouseArea2d.event_while_hovering_occured), this, nameof(_accept_marker_event));
+	    //path_area.Connect(nameof(MouseArea2d.mouse_hover_changed), this, nameof(_render_path_highlight));
+    }
+
+    private void _accept_marker_event(InputEvent ev)
+    {
+        if (!(bool)unit.Get("is_busy"))
+        {
+            unit.Set("mv_adj", this);
+            if ((bool)unit.Call("handle_input", ev))
+            {
+                GetTree().SetInputAsHandled();
+            }
+        }
+    }
+
+    private void _render_marker_highlight()
+    {
+        if ((bool)unit.Get("is_selected") && !(bool)unit.Get("is_busy") && marker.is_mouse_hovering)
+        {
+            marker.GetNode<Sprite>("Sprite").Modulate = C_HIGHLIGHT;
+        }
+        else
+        {
+            marker.GetNode<Sprite>("Sprite").Modulate = C_NOT_HIGHLIGHTED;
+        }
+    }
+
+    private void  _render_path_highlight()
+    {
+        if ((bool)unit.Get("is_selected") && !(bool)unit.Get("is_busy") && path_area.is_mouse_hovering)
+        {
+            unit.Set("high_path", this);
+        }
+    }
+
+    public void enable()
+    {
+        marker.Show();
+        SetProcessInput(true);
+    }
+
+    public void disable()
+    {
+        marker.Hide();
+        SetProcessInput(false);
+    }
+
+    public void update()
+    {
+        GlobalPosition = start + move;
+        var l_vec = ToLocal(start);
+        path.Points = new Vector2[] { l_vec, Vector2.Zero };
+        var shape = new RectangleShape2D();
+        shape.Extents = new Vector2(20, l_vec.Length()/2.0f);
+        path_shape.SetShape(shape);
+        path_shape.Rotation = l_vec.Angle() + Mathf.Pi/2.0f;
+        path_shape.Position = new Vector2(l_vec.x/2.0f, l_vec.y/2.0f);
+        if (next != null)
+            next.update();
+    }
+
+    public void erase()
+    {
+        if (next != null)
+            next.erase();
+        QueueFree();
+    }
+
+    public Vector2 closest_pnt_on_path(Vector2 gpos)
+    {
+        var pnt = ToLocal(gpos);
+        var dir = ToLocal(start).Normalized();
+        var dot = pnt.Dot(dir);
+        return ToGlobal(dir*dot);
+    }
+
+}
--- bgg/decorators/MouseArea2d.cs
+++ bgg/decorators/MouseArea2d.cs
@@ -5,22 +5,23 @@ using System;
 public class MouseArea2d : Area2D
 {
     [Signal]
-    delegate void mouse_hover_changed();
+    public delegate void mouse_hover_changed();
 
     [Signal]
-    delegate void event_while_hovering_occured(InputEvent ev);
+    public delegate void event_while_hovering_occured(InputEvent ev);
 
     public Boolean is_mouse_hovering { get; private set; } = false;
 
     public override void _Ready()
     {
+        base._Ready();
         SetProcessInput(false);
-	    Connect("mouse_entered", this, "OnMouseEnter");
-	    Connect("mouse_exited", this, "OnMouseExit");
-	    Connect("visibility_changed", this, "OnVisibilityChange");
+	    Connect("mouse_entered", this, nameof(OnMouseEnter));
+	    Connect("mouse_exited", this, nameof(OnMouseExit));
+	    Connect("visibility_changed", this, nameof(OnVisibilityChange));
     }
 
-    public override void _Input(InputEvent @event) => EmitSignal("event_while_hovering_occured", @event);
+    public override void _Input(InputEvent @event) => EmitSignal(nameof(event_while_hovering_occured), @event);
 
     private void OnMouseEnter() => MarkAsHovering();
     private void OnMouseExit() => MarkAsNotHovering();
@@ -38,7 +39,7 @@ public class MouseArea2d : Area2D
     {
         is_mouse_hovering = true;
         SetProcessInput(true);
-        EmitSignal("mouse_hover_changed");
+        EmitSignal(nameof(mouse_hover_changed));
     }
 
     // For when markers are manually moved
@@ -46,6 +47,6 @@ public class MouseArea2d : Area2D
     {
         is_mouse_hovering = false;
         SetProcessInput(false);
-        EmitSignal("mouse_hover_changed");
+        EmitSignal(nameof(mouse_hover_changed));
     }
 }
